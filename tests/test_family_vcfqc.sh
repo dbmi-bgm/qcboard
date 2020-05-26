fcc25d945a9fba85fe2497eb46080ab7b88e28af
Daniel Kwon <34282186+danielmsk@users.noreply.github.com>
2020-05-26 17:16:47 -0400

    update

--- tests/test_family_vcfqc.sh
+++ tests/test_family_vcfqc.sh
@@ -1,5 +1,11 @@
 
 
-python ../qcboard.py vcfstat -vcf data/Ashkenazim/HG002.hs37d5.60x.mnvindelmixed.vqsr.vcf.gz data/Ashkenazim/HG003.hs37d5.60x.mnvindelmixed.vqsr.vcf.gz data/Ashkenazim/HG004.hs37d5.60x.mnvindelmixed.vqsr.vcf.gz -ped C,F,M -out HG002.hs37d5.60x.trio
-python ../qcboard.py vcfqc -vcf data/Ashkenazim/HG002.hs37d5.60x.mnvindelmixed.vqsr.vcf.gz data/Ashkenazim/HG003.hs37d5.60x.mnvindelmixed.vqsr.vcf.gz data/Ashkenazim/HG004.hs37d5.60x.mnvindelmixed.vqsr.vcf.gz -ped C,F,M -out HG002.hs37d5.60x.trio
+python ../qcboard.py vcfstat -vcf data/Ashkenazim/HG002.hs37d5.60x.mnvindelmixed.vqsr.vcf.gz \
+	data/Ashkenazim/HG003.hs37d5.60x.mnvindelmixed.vqsr.vcf.gz \
+	data/Ashkenazim/HG004.hs37d5.60x.mnvindelmixed.vqsr.vcf.gz \
+	-ped C,F,M -out HG002.hs37d5.60x.trio
+python ../qcboard.py vcfqc -vcf data/Ashkenazim/HG002.hs37d5.60x.mnvindelmixed.vqsr.vcf.gz \
+	data/Ashkenazim/HG003.hs37d5.60x.mnvindelmixed.vqsr.vcf.gz \
+	data/Ashkenazim/HG004.hs37d5.60x.mnvindelmixed.vqsr.vcf.gz \
+	-ped C,F,M -out HG002.hs37d5.60x.trio
 
